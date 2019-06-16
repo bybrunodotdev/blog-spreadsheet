0649a0906c36779155935ebe442c7c13ed524808
Bruno <hey@brunoandrade.me>
2019-06-16 08:39:09 -0300

    (config) template for practice

--- src/pages/BlogPage/index.js
+++ src/pages/BlogPage/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Tabletop from 'tabletop';
 import Card from '../../components/Common/Card';
 
 import { Container } from './styles';
@@ -7,28 +6,7 @@ import { Container } from './styles';
 import config from '../../config';
 
 class BlogPage extends Component {
-  state = {
-    data: [],
-  };
-
-  componentDidMount() {
-    this.init();
-  }
-
-  init() {
-    Tabletop.init({
-      key: config.KEY_SPREADSHEET,
-      callback: (spreadsheetData) => {
-        this.setState({
-          data: spreadsheetData,
-        });
-      },
-      simpleSheet: true,
-    });
-  }
-
   render() {
-    const { data } = this.state;
     return (
       <Container>
         <div className="circles">
@@ -40,16 +18,6 @@ class BlogPage extends Component {
           <li />
           <li />
         </div>
-
-        {data.map(post => (
-          <Card
-            key={post.id}
-            cover={post.cover}
-            title={post.title}
-            description={post.description}
-            link={post.link}
-          />
-        ))}
       </Container>
     );
   }
